ab118637d269ce9918b554b873e91ead328eb7bf
Ngọc Tưởng <cocghe2468@gmail.com>
2025-08-31 10:38:15 +0700

    update with role

--- MuonTraSach/MuonTraSach/dao.py
+++ MuonTraSach/MuonTraSach/dao.py
@@ -4,16 +4,16 @@ import json, os
 # Không Phân quyền (Lấy file k có role)
 #
 
-def auth_user(username, password):
-    base_dir = os.path.dirname(__file__)
-    file_path = os.path.join(base_dir, "data", "user.json")
-    with open(file_path, encoding="utf8") as f:
-        users = json.load(f)
-        for u in users:
-            if u["username"] == username and u["password"] == password:
-                return u
-    return None
-
+# def auth_user(username, password):
+#     base_dir = os.path.dirname(__file__)
+#     file_path = os.path.join(base_dir, "data", "user.json")
+#     with open(file_path, encoding="utf8") as f:
+#         users = json.load(f)
+#         for u in users:
+#             if u["username"] == username and u["password"] == password:
+#                 return u
+#     return None
+#
 
 
 
@@ -21,15 +21,15 @@ def auth_user(username, password):
 # Phân quyền (Lấy file có role)
 #
 #
-# def auth_user(username, password):
-#     base_dir = os.path.dirname(__file__)
-#     file_path = os.path.join(base_dir, "data", "user_role.json")
-#     with open(file_path, encoding="utf8") as f:
-#         users = json.load(f)
-#         for u in users:
-#             if u["username"] == username and u["password"] == password:
-#                 return u
-#     return None
+def auth_user(username, password):
+    base_dir = os.path.dirname(__file__)
+    file_path = os.path.join(base_dir, "data", "user_role.json")
+    with open(file_path, encoding="utf8") as f:
+        users = json.load(f)
+        for u in users:
+            if u["username"] == username and u["password"] == password:
+                return u
+    return None
 
 
 
--- MuonTraSach/MuonTraSach/index.py
+++ MuonTraSach/MuonTraSach/index.py
@@ -8,33 +8,6 @@ app.secret_key = "secret123"
 #
 # Không phân quyền
 #
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    error = None
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-
-        user = auth_user(username, password)
-        if user:   # đúng username/password
-            session["username"] = user["username"]
-            return redirect(url_for("index"))
-        else:
-            error = "Sai tên đăng nhập hoặc mật khẩu rồi!"
-
-    return render_template("login.html", error=error)
-
-
-@app.route("/")
-def index():
-    if "username" in session:   # nếu đã đăng nhập
-        return render_template("index.html", username=session["username"])
-    return redirect(url_for("login"))
-
-
-#
-# Phân quyền
 #
 # @app.route("/login", methods=["GET", "POST"])
 # def login():
@@ -46,22 +19,50 @@ def index():
 #         user = auth_user(username, password)
 #         if user:   # đúng username/password
 #             session["username"] = user["username"]
-#             session["role"] = user["role"]
 #             return redirect(url_for("index"))
 #         else:
-#             error = "Sai tên đăng nhập hoặc mật khẩu!"
+#             error = "Sai tên đăng nhập hoặc mật khẩu rồi!"
 #
 #     return render_template("login.html", error=error)
 #
+#
 # @app.route("/")
 # def index():
-#     if "username" in session:
-#         if session.get("role") == "admin":
-#             return render_template("admin.html", username=session["username"])
-#         else:
-#             return render_template("index.html", username=session["username"])
+#     if "username" in session:   # nếu đã đăng nhập
+#         return render_template("index.html", username=session["username"])
 #     return redirect(url_for("login"))
 #
+
+#
+# Phân quyền
+#
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = auth_user(username, password)
+        if user:   # đúng username/password
+            session["username"] = user["username"]
+            session["role"] = user["role"]
+            return redirect(url_for("index"))
+        else:
+            error = "Sai tên đăng nhập hoặc mật khẩu!"
+
+    return render_template("login.html", error=error)
+
+@app.route("/")
+def index():
+    if "username" in session:
+        if session.get("role") == "admin":
+            return render_template("admin.html", username=session["username"])
+        else:
+            return render_template("index.html", username=session["username"])
+    return redirect(url_for("login"))
+
 
 
 
